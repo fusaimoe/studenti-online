a750d218e7896c5204266cbc5883fd9e29e27ac5
Andrea Colombo <andread251@gmail.com>
2017-01-05 00:11:21 +0100

    Cleanup

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,6 +5,7 @@ function leftSidebar() {
   $('#sidebar-lb').children().toggle();
   $('#sidebar-rb').children().toggleClass('icon-bell').toggleClass('icon-arrow-right');
 }
+
 function rightSidebar() {
   $('.sidebar-right').toggle();
   $('.row-offcanvas').toggleClass('right');
@@ -12,26 +13,23 @@ function rightSidebar() {
   $('#sidebar-rb').children().toggle();
   $('#sidebar-lb').children().toggleClass('icon-calendar').toggleClass('icon-arrow-left');
 }
+
 function leftButton(close){
   if(close){
     rightSidebar();
-    history.pushState(null, "Returned to index", "index.htm");
     return false;
   } else {
     leftSidebar();
-    history.pushState(null, "Calendar sidebar", "calendar.htm");
-    console.log('ora');
     return true;
   }
 }
+
 function rightButton(close){
   if(close){
     leftSidebar();
-    history.pushState(null, "Returned to index", "index.htm");
     return false;
   } else {
     rightSidebar();
-    history.pushState(null, "Notifications sidebar", "history.htm");
     return true;
   }
 }
@@ -48,13 +46,8 @@ $(document).ready(function () {
     close=rightButton(close)
   });
 
-  if(close){
-    window.onbeforeunload = leftSidebar();
-    close=false;
-  }
-
   /*$(window).on("swipeleft",function(){
     close=leftButton(close);
   });*/
-  
+
 });
