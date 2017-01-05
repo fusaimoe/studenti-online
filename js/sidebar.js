d4e2c6daf83d24c095c72d3be2bb06f76a86be42
Andrea Colombo <andread251@gmail.com>
2017-01-05 22:28:28 +0100

    Mobile swipe

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,6 +7,7 @@ function toggleSidebar(button) {
   $("#sidebar-"+button+"-button").toggleClass('icon-button-disabled');
   $("#sidebar-"+button+"-button").is(':disabled') ? $("#sidebar-"+button+"-button").prop('disabled',false) : $("#sidebar-"+button+"-button").prop('disabled',true);
   $("#sidebar-"+opposite+"-button").children().toggleClass("icon-"+icon).toggleClass("icon-arrow-"+opposite);
+  if(isMobile()) toggleSwipe();
 }
 
 function clickedButton(close, buttonId){
@@ -30,16 +31,51 @@ function getIcon(opposite) {
   return icon = (opposite=="left") ? "calendar" : "bell";
 }
 
+function isMobile() {
+  try{ document.createEvent("TouchEvent"); return true; }
+  catch(e){ return false; }
+}
+
+function toggleSwipe() {
+  if(checkSidebar("left")) {
+    $("body").swipe( {
+      swipeRight:function(event, direction, distance, duration, fingerCount, fingerData) {
+        $("#sidebar-right-button").trigger( "click" );
+      }
+    });
+  } else if(checkSidebar("right")) {
+    $("body").swipe( {
+      swipeLeft:function(event, direction, distance, duration, fingerCount, fingerData) {
+        $("#sidebar-left-button").trigger( "click" );
+      }
+    });
+  } else {
+    $("body").swipe("destroy");
+  }
+}
+
+function checkSidebar(sidebar) {
+  return $("."+sidebar).length;
+}
+
 $(document).ready(function () {
 
   var close = false;
 
+  if(isMobile()){
+
+  }
+
   $('#sidebar-left-button').click(function () {
     close = clickedButton(close, $(this).attr('id'));
+
   });
 
   $('#sidebar-right-button').click(function () {
     close = clickedButton(close, $(this).attr('id'));
   });
+
+
+
 
 });
