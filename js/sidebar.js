75395b3ca8876274c204409d846aee32044dc7f8
Andrea Colombo <andread251@gmail.com>
2017-01-05 01:56:54 +0100

    Homepage fixed

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,7 +2,13 @@ function leftSidebar() {
   $('.sidebar-left').toggle();
   $('.row-offcanvas').toggleClass('left');
   $('.main-offcanvas').toggle();
-  $('#sidebar-lb').children().toggle();
+  $('#sidebar-lb').children().visibilityToggle();
+  $('#sidebar-lb').toggleClass('icon-button-disabled');
+  if ($('#sidebar-lb').is(':disabled')) {
+    $('#sidebar-lb').prop('disabled',false)
+  } else {
+    $('#sidebar-lb').prop('disabled',true);
+  }; // This causes Bug 685657 on Firefox though, after refresh it will stay disabled
   $('#sidebar-rb').children().toggleClass('icon-bell').toggleClass('icon-arrow-right');
 }
 
@@ -10,7 +16,13 @@ function rightSidebar() {
   $('.sidebar-right').toggle();
   $('.row-offcanvas').toggleClass('right');
   $('.main-offcanvas').toggle();
-  $('#sidebar-rb').children().toggle();
+  $('#sidebar-rb').children().visibilityToggle();
+  $('#sidebar-rb').toggleClass('icon-button-disabled');
+  if ($('#sidebar-rb').is(':disabled')) {
+    $('#sidebar-rb').prop('disabled',false);
+  } else {
+    $('#sidebar-rb').prop('disabled',true);
+  }; // This causes Bug 685657 on Firefox though, after refresh it will stay disabled
   $('#sidebar-lb').children().toggleClass('icon-calendar').toggleClass('icon-arrow-left');
 }
 
@@ -49,5 +61,19 @@ $(document).ready(function () {
   /*$(window).on("swipeleft",function(){
     close=leftButton(close);
   });*/
+
+  jQuery.fn.visible = function() {
+    return this.css('visibility', 'visible');
+  };
+
+  jQuery.fn.invisible = function() {
+    return this.css('visibility', 'hidden');
+  };
+
+  jQuery.fn.visibilityToggle = function() {
+    return this.css('visibility', function(i, visibility) {
+        return (visibility == 'visible') ? 'hidden' : 'visible';
+    });
+  };
 
 });
