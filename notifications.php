7a73a5e7e485f13da5d4a2d54ff5189855ed140e
Giulia Cecchetti <giulia.cecchetti96@gmail.com>
2017-01-12 23:41:13 +0100

    Notifications fixing

--- notifications.php
+++ notifications.php
@@ -3,40 +3,36 @@
   <div class="card card-block">
     <?php
 
-    $sql = "SELECT id, student_id, pub_date, category_name, subject, description FROM notifications WHERE (student_id = '" . $_SESSION['student_id'] ."')";
+    $sql = "SELECT id, student_id, pub_date, category_name, subject, description, read_flag FROM notifications WHERE (student_id = '" . $_SESSION['student_id'] ."')";
     $result = $mysqli->query($sql);
 
-      if ($result->num_rows > 0) {
-        echo '<ul class="list-group history">';
+    if ($result->num_rows > 0) {
+      echo '<ul class="list-group history">';
+      while($row = $result->fetch_assoc()) {
+        $subject = $row['subject'];
+        $description = $row['description'];
+        $category_name = $row['category_name'];
+        $pub_date = $row['pub_date'];
+        $read_flag = $row['read_flag'];
+        $categoryCss = str_replace(' ', '', strtolower($category_name));
 
-        while($row = $result->fetch_assoc()) {
-
-          $subject = $row['subject'];
-          $description = $row['description'];
-          $category_name = $row['category_name'];
-          $pub_date = $row['pub_date'];
-
-          $categoryCss = str_replace(' ', '', strtolower($category_name));
-
-          echo '
-          <li class="list-group-item list-group-notification notification-unread">
-
-            <div class="tag-align">
-              <span class="tag tag-custom bg-color-' . $categoryCss . '">
-                <span class="icon-custom-' . $categoryCss . '" aria-hidden="true"></span>
-              </span>
-            </div>
-            <span>
-              <strong>' . $subject . '</strong> - ' . $description . '</br>
-              <span class="text-muted">' . $pub_date . ' · ' . $category_name . '</span>
+        echo '<li class="list-group-item list-group-notification';
+        if(!$read_flag) echo ' notification-unread';
+        echo ' ">
+          <div class="tag-align">
+            <span class="tag tag-custom bg-color-' . $categoryCss . '">
+              <span class="icon-custom-' . $categoryCss . '" aria-hidden="true"></span>
             </span>
-
-          </li>
-          ';
+          </div>
+          <span>
+            <strong>' . $subject . '</strong> - ' . $description . '</br>
+            <span class="text-muted">' . $pub_date . ' · ' . $category_name . '</span>
+          </span>
+        </li>';
 
         }
-
         echo '</ul>';
+
       } else {
         echo 'no notifications'; //TODO do it better
       }
