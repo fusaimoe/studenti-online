fbfe9262f2e43500834ecf8fbd883faa5a55ffa3
Giulia Cecchetti <giulia.cecchetti96@gmail.com>
2017-01-13 13:49:14 +0100

    Fixing

--- php/update_calendar.php
+++ php/update_calendar.php
@@ -1,48 +1,20 @@
+
 <?php
-
-  /*
-  $categories = $_POST['categories'];
-
-  $in = join(',', array_fill(0, count($categories), '?'));
-  */
-
   $sql = "SELECT e.id, e.name, e.start_date, e.end_date, e.URL, e.category_name, c.color FROM calendar_events e, categories c
-          WHERE e.category_name=c.name /* AND e.category_name IN ($in) */
+          WHERE e.category_name=c.name
           AND e.student_id='" . $_SESSION['student_id'] ."'";
-
-  /* //WATTTTTTTTTT?
-  $select = <<<SQL
-    SELECT *
-    FROM galleries
-    WHERE id IN ($in);
-  SQL;
-  */
-
-  /*
-  $statement = $mysqli->prepare($select);
-  $statement->bind_param(str_repeat('s', count($categories)), ...$categories);
-  $statement->execute();
-  $result = $statement->get_result();
-  */
   $result = $mysqli->query($sql);
-
   $xml = new XMLWriter();
-
   $xml->openURI("events.xml");
   $xml->startDocument('1.0');
   $xml->setIndent(true);
-
   $xml->startElement('monthly');
-
   if ($result->num_rows > 0) {
-
     while($row = $result->fetch_assoc()) {
-
       $startDate = date("Y-m-d", strtotime($row['start_date']));
       $startTime = date("H:i", strtotime($row['start_date']));
       $endDate = date("Y-m-d", strtotime($row['end_date']));
       $endTime = date("H:i", strtotime($row['end_date']));
-
       $xml->startElement("event");
         $xml->writeElement('id', $row['id']);
         $xml->writeElement('name', $row['name']);
@@ -55,8 +27,6 @@
       $xml->endElement();
     }
   }
-
   $xml->endElement();
   $xml->flush();
-
 ?>
